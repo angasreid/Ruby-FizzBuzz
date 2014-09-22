c1b014c95303502cfabbeb074446995c1b46591a
Angas <angasreid@gmail.com>
2014-09-22 17:01:12 -0400

    FizzBuzz by Angas

---
+++ fizzbuzz.rb
@@ -0,0 +1,21 @@
+
+#Angas' Ruby Fizz
+
+1.upto(100) do |number|
+	if number % 3 == 0 && number % 5 == 0
+		puts "Fizz Buzz"
+
+	elsif number % 3 ==0
+		puts "Fizz"
+
+	elsif number % 5 ==0 
+		puts "Buzz"
+
+	elsif
+	puts "#{number}"
+	number += number
+	end
+end
+
+
+
